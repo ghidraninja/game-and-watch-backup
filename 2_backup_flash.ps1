9578fa2a18b3e7aa42b66cd1573b0866a61fbacf
unknown <lou1979v@gmail.com>
2020-11-30 03:12:04 +0100

    Tided up initial mess

--- 2_backup_flash.ps1
+++ 2_backup_flash.ps1
@@ -6,7 +6,6 @@ $path = $PSScriptRoot + "\backups"
 if (!(Test-Path -Path $path)) {
   New-Item -Path $PSScriptRoot -Name "backups" -ItemType "directory"
 }
-#Write-Host "Script file is in:" $path
 
-openocd-0.10.0-15/bin/openocd.exe -s $PSScriptRoot"\openocd" -f flash_"$adapter".cfg 
+openocd-0.10.0-15_win/bin/openocd.exe -s $PSScriptRoot"\openocd" -f flash_"$adapter".cfg 
 Pause
