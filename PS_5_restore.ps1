eb5ffa7cab24ce99b99d0f4964bbb17265a5c1f3
unknown <lou1979v@gmail.com>
2020-12-06 22:28:06 +0100

    fix adapter bug

--- PS_5_restore.ps1
+++ PS_5_restore.ps1
@@ -3,7 +3,7 @@ Clear-Host
 Write-Host "Usage: <Adapter: jlink or stlink>"
 $adapter = Read-Host
 $Loc = $PSScriptRoot
-$Interface_cfg = "$Loc\openocd\interface_" + $1 + ".cfg"
+$Interface_cfg = "$Loc\openocd\interface_" + $adapter + ".cfg"
 $backup = "$Loc\backups\internal_flash_backup.bin"
 if(!(Test-Path "$Loc\logs\")){
   New-Item -Path $PSScriptRoot -Name "logs" -ItemType "directory"
@@ -23,13 +23,11 @@ Write-Host "Ok, restoring original firmware! (We will not lock the device, so yo
 
 Write-Host "Restoring internal flash..."
 
-<# #Invoke-Expression "
-openocd -f $Interface_cfg -c "init;" -c "halt;" -c "program internal_flash_backup.bin 0x08000000 verify;" -c "exit;" *>&1  
-#| Out-File "$Loc\logs\5_openocd.log" -Encoding ascii -Append
+Invoke-Expression "openocd -f $Interface_cfg -c 'init;' -c 'halt;' -c 'program $backup 0x08000000 verify;' -c 'exit;'" *>&1  | Out-File "$Loc\logs\5_openocd.log" -Encoding ascii -Append
 if(-not $LASTEXITCODE -eq 0){
     Write-Host "Restoring internal flash failed. Check debug connection and try again."
     break
-} #>
+}
 
 
 Write-Host "Restoring SPI flash..."
