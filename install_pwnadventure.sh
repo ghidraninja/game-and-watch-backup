a723e972d4121de69cbc93531738dc42388fa4ac
Thomas Roth <thomas.roth@leveldown.de>
2020-12-08 13:42:29 +0100

    Update install_pwnadventure.sh

--- install_pwnadventure.sh
+++ install_pwnadventure.sh
@@ -14,6 +14,9 @@ fi
 
 
 echo "Installing data on SPI flash..."
+
+# If you adjust this line to point to a different binary, make sure to use the .elf, not the .bin file!
+# I.e.: ./scripts/flashloader.sh $ADAPTER ../game-and-watch-retro-go/build/gw_retrogo_nes_extflash.elf
 if ! ./scripts/flashloader.sh $ADAPTER prebuilt/gw_retrogo_nes_extflash.bin; then
     echo "Installing on SPI flash failed. Check debug connection and try again."
     exit 1
