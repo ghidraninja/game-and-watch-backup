b9d763bf1004a7dd2e0355536ef9fffd7234a457
sds1979v <lou1979v@gmail.com>
2020-12-21 22:02:42 +0100

    new XPACK OpenOCD version

--- PS_0_installtools.ps1
+++ PS_0_installtools.ps1
@@ -1,6 +1,7 @@
 Clear-Host
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install openocd -y
+choco install openocd-xpack -y
 choco install dd -y
 choco install gcc-arm-embedded -y
+choco install python -y
